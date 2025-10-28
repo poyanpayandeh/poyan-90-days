642037bf5801818fe57770ece230f78aff59f5e9
Poyan <poyanpayandeh@hotmail.com>
2025-10-29 06:41:15 +1300

    Added FizzBuzz Exercise

---
+++ day2-csharp-basics/FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+ 
+static void Main(string[] args) {
+  for (int i = 0; i <= 100; i++) {
+      if(i % 3 == 0){
+        Console.Write("Fizz");
+      }
+
+      if (i % 5 == 0){
+        Console.Write("Buzz");
+      }
+      
+      if (i % 3 != 0 && i % 5 != 0){
+        Console.Write(i);
+      }
+
+      Console.Write("\n");
+    }
+  }
+}
+
+
+
