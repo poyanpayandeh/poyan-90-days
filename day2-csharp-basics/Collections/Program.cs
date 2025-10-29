009ce894ab2a6f0ec495df910d7ed571c7f647e6
Poyan <poyanpayandeh@hotmail.com>
2025-10-29 21:27:07 +1300

    Added List Exercise

---
+++ day2-csharp-basics/Collections/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> list = new List<int>();
+
+for (int i = 1; i <= 10; i++){
+  list.Add(i);
+}
+
+Console.WriteLine($"Min {list.Min()}");
+Console.WriteLine($"Max: {list.Max()}");
+Console.WriteLine($"Average: {list.Average()}");
